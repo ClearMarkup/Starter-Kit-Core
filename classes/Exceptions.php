29ac1fae8e412b8d8b403841795fce9f8562d699
Nabil <nabil.noel.m@gmail.com>
2024-03-01 02:07:34 +0100

    Refactor exception handling in Exceptions class

--- classes/Exceptions.php
+++ classes/Exceptions.php
@@ -16,16 +16,14 @@ class Exceptions
      */
     public function getMessage($exception)
     {
-        switch (get_class($exception)) {
-            case 'Delight\Auth\InvalidEmailException':
-            case 'Delight\Auth\InvalidPasswordException':
-                return _('Invalid email or password');
-            case 'Delight\Auth\EmailNotVerifiedException':
-                return _('Email not verified');
-            case 'Delight\Auth\TooManyRequestsException':
-                return _('Too many requests have been made');
-            default:
-                return _('An error occurred, please try again later.');
+        if ($exception instanceof \InvalidArgumentException) {
+            return 'Invalid argument';
+        } elseif ($exception instanceof \RuntimeException) {
+            return 'Runtime error';
+        } elseif ($exception instanceof \Exception) {
+            return 'General exception';
+        } else {
+            return 'Unknown error';
         }
     }
 }
